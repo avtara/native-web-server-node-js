3d2e60f61042490e3adaef6cf58ad5cb66b35c10
avtar <avtarakhrisna1@gmail.com>
2021-07-04 00:49:15 +0700

    feat: learn make simple http server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("initial commit");
+const http = require('http');
+
+const requestListener = (request,response) =>{
+    response.setHeader('Content-Type', 'text/html');
+
+    response.statusCode = 200;
+    response.end('<h1>halo HTTP server!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port,host,()=>{
+    console.log(`server berjalan pada http://${host}:${port}`)
+})
